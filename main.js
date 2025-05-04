3f2cfa00aae12996af0ae7f76dfbe265aa8105c5
karlo-kasi <karlokasi1995@gmail.com>
2025-05-04 19:48:43 +0200

    snack 3

--- main.js
+++ main.js
@@ -87,3 +87,22 @@ const fullPricedBook = discountedBooks.find((n) => {
 })
 
 console.log(fullPricedBook)
+
+/*Snack 3 - Ordinare gli Autori
+Creare un array (authors) che contiene gli autori dei libri.
+Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+Ordina l’array authors in base all’età, senza creare un nuovo array.
+(se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)*/
+
+const authors = books.map((b) => b.author)
+
+console.log(authors)
+
+const areAuthorsAdults = authors.every((b) => b.età > 18)
+
+console.log(areAuthorsAdults)
+
+authors.sort((a, b) => areAuthorsAdults ? a.age - b.age : b.age - a.age )
+
+
+
